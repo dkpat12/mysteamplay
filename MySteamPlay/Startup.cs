05724c707ef02715e437f957834715b25482c58f
Dillion Patrick <dkpat12@gmail.com>
2015-04-17 15:49:05 -0400

    Initial Visual Studio project
    Blank mvc.net 5 application created

---
+++ MySteamPlay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MySteamPlay.Startup))]
+namespace MySteamPlay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
