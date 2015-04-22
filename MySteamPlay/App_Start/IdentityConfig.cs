adc156cda0b0b5eba7627916c015c43e2978f150
Dillion Patrick <dkpat12@gmail.com>
2015-04-22 16:01:11 -0400

    Changed Password requirements
    passwords are no longer required to contain a special character, but
    must be 8 characters.

--- MySteamPlay/App_Start/IdentityConfig.cs
+++ MySteamPlay/App_Start/IdentityConfig.cs
@@ -53,8 +53,8 @@ namespace MySteamPlay
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequiredLength = 8,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
