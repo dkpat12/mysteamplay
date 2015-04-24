cf6a7ac470ba5629ce3fb02636a2a5e30a9502f5
JSSch <smartblue@gmail.com>
2015-04-24 18:16:26 -0400

    API working, user games db storage working, huzzah!

--- MySteamPlay/Models/Game.cs
+++ MySteamPlay/Models/Game.cs
@@ -8,13 +8,12 @@ namespace MySteamPlay.Models
 {
     public class Game
     {
-        [Key]
+        public int ID { get; set; }  
+
         public int appID { get; set; }              // should be unique identifier for each game, consistent variable naming with Steam API
         public string name { get; set; }            // game name, the variable is named consistently with the Steam API
         public string img_icon_url { get; set; }    // game icon link
         public string img_logo_url { get; set; }    // game logo link
-        public virtual TagList Tags { get; set; }
-
         public virtual List<GameDescrip> GameDescriptions{ get; set; }
     }
 }
--- MySteamPlay/Models/GameDescrip.cs
+++ MySteamPlay/Models/GameDescrip.cs
@@ -8,11 +8,13 @@ namespace MySteamPlay.Models
     public class GameDescrip
     {
         public int ID { get; set; }
-        public ulong steamID { get; set; }          // foreign key from User, part of composite key
+        public string userId { get; set; }          
         public int appID { get; set; }              // foreign key from Game, part of composite key
         public int playtime_forever { get; set; }   // all the time the owner has ever spent playing this game
         public string userComments { get; set; }    // comments by the owner
         public bool visible { get; set; }           // if true, game appears in owner's master list
+
+        public virtual TagList Tags { get; set; }
 
         public virtual Game Game { get; set; }
     }
